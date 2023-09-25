651c0289671310b94c39659e0e798d8d6d323f87
Nwnaas <113027006+Nwnaas@users.noreply.github.com>
2023-09-25 08:04:47 +0000

    coins

--- nav.php
+++ nav.php
@@ -8,37 +8,6 @@
     <link rel="stylesheet" href="/bigwin/nav.css" />
 </head>
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyB3PWjhLgvX8yvbA3OcJP3qoS3-tp8d_BU",
-    authDomain: "bigwin-a63cb.firebaseapp.com",
-    databaseURL: "https://bigwin-a63cb-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bigwin-a63cb",
-    storageBucket: "bigwin-a63cb.appspot.com",
-    messagingSenderId: "524008852003",
-    appId: "1:524008852003:web:e9cdcf404ae8e1fc16bf96",
-    measurementId: "G-6FVGVMWTFW"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
-  import {getDatabase, ref, get, set, child, update, remove} 
-  from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
-
-  const db = getDatabase();
-
-  var namebox = document.getElementById("nameBox")
-</script>
-
 <body>
     <nav class="navbar">
         <a href="home.php" class="nav-branding"> <img style="border:0px solid black;" src="Logo/BigWin3.png"
@@ -60,7 +29,7 @@
         </ul>
         <div class="login-container">
             <li class="nav-item">
-                <div id="coinCount"><p id="coins">10</p></div>
+                <div id="coinCount"><p id="coins"><?php echo $_SESSION['coins']; ?></p></div>
             </li>
         
             <li class="nav-item">
