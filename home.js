439ee8754106570cee77a59feacaeb3a390c8034
Nwnaas <113027006+Nwnaas@users.noreply.github.com>
2023-09-25 09:08:09 +0000

    fiks

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 let coinsdoc = document.getElementById("coins");
 var precoins = coinsdoc.innerHTML;
-let coins = int(precoins); // Initial number of coins 
+let coins = precoins; // Initial number of coins 
 console.log(coins + " er på mongomåten")
