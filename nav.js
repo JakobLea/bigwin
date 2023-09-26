356df0f1d497797bb1b94f34252d5d493d6c749f
Nwnaas <113027006+Nwnaas@users.noreply.github.com>
2023-09-26 06:55:12 +0000

    slettet nav filen

--- nav.js
+++
@@ -1,7 +0,0 @@
-let coinsdoc = document.getElementById("coin");
-var precoins = coinsdoc.innerHTML;
-let coins = precoins; // Initial number of coins 
-
-coinsdoc.remove();
-
-console.log(coins + " er på mongomåten")
