2de17d536fb271a855ba57d7e2ec5b9f270acaca
goldfirework <jthallaug@gmail.com>
2023-09-27 22:57:31 +0200

    Prøver no fort :)

--- station.php
+++ station.php
@@ -9,7 +9,7 @@ $result = mysqli_query($conn, "SELECT * FROM users WHERE id='" . $id . "';");
 
 while ($row = mysqli_fetch_array($result))
     {
-    echo "<p class='coins'>" . $row['coins'] . "</p>";
+    echo "<p class='coins' id='coins'>" . $row['coins'] . "</p>";
     }
 
 mysqli_close($conn);
