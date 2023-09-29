abc22986c6bd74e71a5f4cfcdd0ce6c934e5a388
Nwnaas <113027006+Nwnaas@users.noreply.github.com>
2023-09-29 07:33:06 +0000

    ;

--- mines.php
+++ mines.php
@@ -246,7 +246,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
             let mines = [];
             let points = 0;
-            let coins = document.getElementById("coinCount").innerHTML
+            let coins = document.getElementById("coinCount").innerHTML;
 
 
             let gameStarted = false; // Flag to track if the game has started
