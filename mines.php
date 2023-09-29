e631883d1fb017c802693a356da7fd89d5feaa43
“JakobLea” <jakob.lea@outlook.com>
2023-09-29 13:05:36 +0200

    int

--- mines.php
+++ mines.php
@@ -247,7 +247,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
             let mines = [];
             let points = 0;
-            let coins = document.getElementById("coinCount").innerHTML;
+            let coins = parseInt(document.getElementById("coinCount").innerHTML);
 
 
             let gameStarted = false; // Flag to track if the game has started
