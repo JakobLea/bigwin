04270722e7fb13d58ffc74a8d8dfab0ae057d005
“JakobLea” <jakob.lea@outlook.com>
2023-09-29 13:04:11 +0200

    push

--- mines.php
+++ mines.php
@@ -247,7 +247,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
             let mines = [];
             let points = 0;
-            let coins = 10;
+            let coins = document.getElementById("coinCount").innerHTML;
 
 
             let gameStarted = false; // Flag to track if the game has started
@@ -308,7 +308,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
             function startGame() {
                 if (!gameStarted) {
-                    loadstasion();
+                    // loadstasion();
                     const startGameSound = document.getElementById("startGameSound");
                     startGameSound.play();
                     const coinsToSpendInput = document.getElementById("boxforinp");
