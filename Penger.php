f8b5a61afd8276589662208eb901227fe1845ad6
Nwnaas <113027006+Nwnaas@users.noreply.github.com>
2023-10-01 14:44:16 +0000

    glemte å linke CSS

--- Penger.php
+++ Penger.php
@@ -11,6 +11,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Daglig Penger</title>
+        <link rel="stylesheet" href="nav.css">
     </head>
 
 
