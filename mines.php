27f35c9a85d9b56835e0ac25adb22059be63b9b4
Nwnaas <113027006+Nwnaas@users.noreply.github.com>
2023-10-16 10:00:25 +0000

    fikkset litt på layoutet

--- mines.php
+++ mines.php
@@ -18,40 +18,6 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
     <script src="jonsfiler/jquery-3.7.1.min.js"></script>
 
-    <!-- <script>
-        function includeHTML() {
-            var z, i, elmnt, file, xhttp;
-            /*loop through a collection of all HTML elements:*/
-            z = document.getElementsByTagName("*");
-            for (i = 0; i < z.length; i++) {
-                elmnt = z[i];
-                /*search for elements with a certain attribute:*/
-                file = elmnt.getAttribute("w3-include-html");
-                if (file) {
-                    /*make an HTTP request using the attribute value as the file name:*/
-                    xhttp = new XMLHttpRequest();
-                    xhttp.onreadystatechange = function() {
-                        if (this.readyState == 4) {
-                            if (this.status == 200) {
-                                elmnt.innerHTML = this.responseText;
-                            }
-                            if (this.status == 404) {
-                                elmnt.innerHTML = "Page not found.";
-                            }
-                            /*remove the attribute, and call this function once more:*/
-                            elmnt.removeAttribute("w3-include-html");
-                            includeHTML();
-                        }
-                    }
-                    xhttp.open("GET", file, true);
-                    xhttp.send();
-                    /*exit the function:*/
-                    return;
-                }
-            }
-        };
-    </script> -->
-
     <body style="background-color:#333333">
         <nav class="navbar">
             <a href="home.php" class="nav-branding"> <img style="border:0px solid black;" src="Logo/BigWin3.png" width=100px, height=50px></a>
